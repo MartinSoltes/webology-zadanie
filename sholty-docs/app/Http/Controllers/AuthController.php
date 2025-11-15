212a9cb22bb61221da6044aacd40dd48fc5fb93a
Martin Šoltés <martinsoltes@Martins-Mac-mini.local>
2025-11-15 10:21:53 +0100

    init laravel

---
+++ sholty-docs/app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string|min:4',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json(['message' => 'User created successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['name', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'user' => auth()->user()
+        ]);
+    }
+}
