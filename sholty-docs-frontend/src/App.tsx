0b8713f2d28a8561c08b5c877ccbeba349ae1d15
Martin Šoltés <martinsoltes@Martins-Mac-mini.local>
2025-11-16 09:25:03 +0100

    add frontend

---
+++ sholty-docs-frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-emerald-400">
+        Tailwind funguje 🎉
+      </h1>
+    </div>
+  );
+}
